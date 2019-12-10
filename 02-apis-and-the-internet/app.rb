d54083f81a3f252ef56c5baf053e45a06b9f2a49
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-12-10 09:22:14 -0500

    prelecture

---
+++ 02-apis-and-the-internet/app.rb
@@ -0,0 +1,6 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
+GOOGLE_BOOKS_API_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="
+
---
+++ 02-apis-and-the-internet/donow.rb
@@ -0,0 +1,36 @@
+
+# For each of the following examples, write two methods that solve the 
+# problem:  One that uses `.each` and one that uses one of the higher level 
+# iterator methods
+
+# Remember:
+
+# map/collect:  Returns an array where each value of the original array 
+# corresponds to an element in the new array
+
+# find/detect:  Returns the first element for which the statement in the block
+# evaluates as True
+
+# select/find_all:  Returns an array of _all_ elements for which the statement in
+# the block evaluates as True
+
+nums = (1..20).to_a #creates an array of all numbers from 1-20
+
+# 1.  Create an array of numbers where each new number is three times as big as
+# its original number (e.g., [3, 6, 9])
+
+
+# 2.  Find the first number that is divisible by 7
+
+
+# 3.  Find all numbers that are divisible by 7
+
+
+# 4.  Find the first number that is divisible by 7 AND greater than 10
+
+
+# Bonus:
+
+
+# 5.  Create an array of the squares (the number times itself) of all numbers
+# that are divisible by 7
