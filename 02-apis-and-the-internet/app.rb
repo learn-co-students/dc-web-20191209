26f0169087880fdfef880cb4d3aca9f4c1765cc4
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-12-10 14:44:22 -0500

    post lecture 02

--- 02-apis-and-the-internet/app.rb
+++ 02-apis-and-the-internet/app.rb
@@ -4,3 +4,66 @@ require 'pry'
 
 GOOGLE_BOOKS_API_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="
 
+def welcome_user
+    system "clear"
+    puts "Welcome to our book searcher"
+end
+
+def get_search_terms
+    puts "What would you like to search for?"
+    search_terms = gets.chomp
+    if search_terms.empty?
+        puts "You must enter something"
+        get_search_terms
+    else
+        return search_terms
+    end
+end
+
+def build_url(search_terms)
+    formatted_search_terms = search_terms.gsub(" ", "+")
+    url = GOOGLE_BOOKS_API_BASE_URL + formatted_search_terms
+    return url
+end
+
+def make_request(url)
+    response = RestClient.get(url)
+end
+
+def parse_response_to_json(response)
+    JSON.parse(response)
+end
+
+def get_first_ten_books(json)
+    # return an array of hashes of at most ten items
+    # [{title: title1, author: author1}, {title: title2, author: author2}]
+    first_ten = json["items"][0..10]
+    first_ten.collect do |book|
+        {title: book["volumeInfo"]["title"], author: book["volumeInfo"]["authors"]}
+    end
+end
+
+def print_book(book)
+    # takes in hash of {title: title1, author: author1} 
+    # outputs #{title1} by #{author1}
+    author_array = book[:author]
+    author_array ||= ["Unknown"]
+    author_string = author_array.join(" & ")
+    puts "#{book[:title]} by #{author_string}"
+    puts "********************\n"
+end
+
+def run 
+    welcome_user
+    search_terms = get_search_terms
+    url = build_url(search_terms)
+    response = make_request(url)
+    json = parse_response_to_json(response)
+    first_ten = get_first_ten_books(json)
+    first_ten.each {|book| print_book(book)}
+
+end
+
+
+
+run
--- 02-apis-and-the-internet/donow.rb
+++ 02-apis-and-the-internet/donow.rb
@@ -15,22 +15,95 @@
 # the block evaluates as True
 
 nums = (1..20).to_a #creates an array of all numbers from 1-20
-
 # 1.  Create an array of numbers where each new number is three times as big as
 # its original number (e.g., [3, 6, 9])
+
+def three_times_each(numbers)
+    answer = []
+    numbers.each do |num|
+        answer << num * 3
+    end
+    return answer
+end
+
+# print three_times_each(nums)
+
+def three_times_map(numbers)
+    numbers.map {|num| num * 3 }
+end
+
+# print three_times_map(nums)
 
 
 # 2.  Find the first number that is divisible by 7
 
+def divis_by_7_each(nums)
+    nums.each do |num|   
+        if num % 7 == 0
+            return num
+        end
+    end
+end
+
+# print divis_by_7_each(nums)
+
+def divis_by_7_find(nums)
+    nums.find do |num|
+        num % 7 == 0
+    end
+end
+
+# print divis_by_7_find(nums)
 
 # 3.  Find all numbers that are divisible by 7
 
+def all_divis_by_7(nums)
+    nums.select do |num|
+        num % 7 == 0
+    end
+
+end
+
+# print all_divis_by_7(nums)
 
 # 4.  Find the first number that is divisible by 7 AND greater than 10
 
+def big_divis_by_7(nums)
+    my_number = nums.find do |num|
+        num % 7 == 0 && num > 10
+    end
+    puts "My number is #{my_number}"
+    return my_number
 
+end
+
+# print big_divis_by_7(nums)
 # Bonus:
 
 
 # 5.  Create an array of the squares (the number times itself) of all numbers
 # that are divisible by 7
+
+def squares_divis_by_7(nums)
+    # find the numbers divisible by 7
+    # multiply each one by itself (i.e., square it)
+    nums.select do |num|
+        num % 7 == 0
+    end.map do |num|
+        num ** 2
+    end
+
+end
+
+print squares_divis_by_7(nums)
+
+
+
+
+
+
+
+
+
+
+
