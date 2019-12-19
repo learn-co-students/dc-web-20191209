cbe83fe531322c0ee11a34b8b28b268fed67e1b4
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-12-19 11:44:19 -0500

    post lecture 09

--- 09-intro-to-orms/config/environment.rb
+++ 09-intro-to-orms/config/environment.rb
@@ -1,7 +1,25 @@
 require 'bundler'
 Bundler.require
+require_relative '../lib/book.rb'
+require_relative '../lib/author.rb'
 
 DB = SQLite3::Database.new('db/library.db')
 
-require_relative '../lib/book.rb'
-require_relative '../lib/author.rb'
+
+# module SQLite3
+
+#     class Database
+
+#         def initialize(file_path)
+#             @file_path = file_path
+#             # set up actual database
+
+#         end
+
+#         def execute(sql)
+#             # executes the sql against the database
+
+#         end
+#     end
+
+# end
--- 09-intro-to-orms/lib/author.rb
+++ 09-intro-to-orms/lib/author.rb
@@ -1,3 +1,62 @@
 class Author
 
+    attr_accessor :name, :id
+
+    def initialize(name, id=nil)
+        @name = name
+        @id = id
+    end
+
+    def save
+        if self.id.nil?
+            sql = <<-SQL
+                INSERT INTO authors (name) VALUES (?)
+            SQL
+            DB.execute(sql, self.name)
+            sql = "SELECT last_insert_rowid()"
+            self.id = DB.execute(sql)[0][0]
+        else
+            sql = <<-SQL
+                UPDATE authors set name = ? WHERE id = ?
+            SQL
+            DB.execute(sql, self.name, self.id)
+        end
+        return self
+    end
+
+    def self.create(name: )
+        # Author.create(name: "Maya Angelou") 
+        # >> write to the database  
+        # >> return Author objects
+        author = Author.new(name)
+        author.save
+        return author
+    end
+
+    def self.find(id)
+        sql = <<-SQL
+            SELECT * FROM authors
+            WHERE id = ?
+        SQL
+        authors_array = DB.execute(sql, id)
+        author_array = authors_array[0] # [3, "J.K. Rowling"]
+        self.make_author_from_row(author_array)
+    end
+
+    def self.all
+        # returns all Authors
+        sql = <<-SQL
+            SELECT * FROM authors 
+        SQL
+        author_array = DB.execute(sql) # [[1, "Maya Angelou"], [2, "Stephen King"]]
+        author_array.map do |row|
+            self.make_author_from_row(row)
+        end
+    end
+
+    def self.make_author_from_row(author_array)
+        author_name = author_array[1]
+        author_id = author_array[0]
+        Author.new(author_name, author_id)
+    end
 end
