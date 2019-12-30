0a2d35d88910cd7fe087957f0900440710d9ffb6
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-12-30 15:19:54 -0500

    hi

--- code-challenge-review/walker.rb
+++ code-challenge-review/walker.rb
@@ -2,9 +2,14 @@ class Walker
 
     attr_accessor :name
     attr_reader :birthday 
-    
+
     def initialize(name, birthday)
         @name, @birthday = name, birthday
     end
 
+
+    def dogs
+        # returns array of all dogs this walker has walked
+
+    end
 end
