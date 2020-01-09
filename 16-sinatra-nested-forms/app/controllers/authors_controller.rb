e0100de58b2e9cbb6f7ece1e5f89f476d55300b9
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-01-09 11:56:52 -0500

    post lecture

--- 16-sinatra-nested-forms/app/controllers/authors_controller.rb
+++ 16-sinatra-nested-forms/app/controllers/authors_controller.rb
@@ -9,9 +9,39 @@ class AuthorsController < Sinatra::Base
         erb :index
     end
 
-    get "/authors/:id" do 
-        @author = Author.find(params[:id])
+
+    get %r{/authors/([\d]+)} do 
+        id = params[:captures][0]
+        @author = Author.find(id)
         erb :show
+
+    end
+
+    get "/authors/new" do 
+        @one = "1"
+        @hello = "hi"
+        erb :new
+    end
+
+    get "/authors/:name" do 
+        slug = params[:name]
+        name = slug.gsub("_", " ").titlecase
+        @author = Author.find_by(name: name)
+        erb :show
+
+    end
+
+    post "/authors" do 
+
+        author_name = params[:name]
+        author = Author.find_or_create_by(name: author_name)
+        params["book"].each do |book_hash|
+            title = book_hash[:title]
+            snippet = book_hash[:snippet]
+            Book.find_or_create_by(title: title, author: author, snippet: snippet)
+        end
+        redirect ("/authors/#{author.id}")
+
 
     end
 end
