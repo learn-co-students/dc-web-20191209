0486e2a2119563ab67c0f57e986a9bf0f463dc51
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-02-04 10:49:58 -0500

    renamed lecture

--- 32-rails-api-pt1/cats-frontend/src/finishedIndex.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    fetchHobbies()
-    fetchCats()
-    catForm().addEventListener("submit", createCat)
-})
-
-function catForm() {
-    return document.querySelector("form")
-}
--- 32-rails-api-pt1/cats-frontend/src/index.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-  console.log('DOM is loaded')
-  console.log('This app uses Semantic UI for styling')
-})
